e6d8b62946722e3a7167cf88b27998eade8f6ca1
Vaibhav Acharya <vaibhavacharya111@gmail.com>
2022-12-11 20:12:28 +0530

    pikachu

---
+++ README.md
@@ -0,0 +1,20 @@
+# [Gist Snip](https://github.com/VaibhavAcharya/gist-snip) — Make your gists actually useful. 🚀
+
+GistSnip is an extension for [VS Code](https://code.visualstudio.com) that lets you **access your GitHub gists as autocomplete snippets** within the code editor.
+
+With GistSnip, you can:
+- 🔍 Easily access all your GitHub gists from within VS Code
+- 💡 Use your gists as autocomplete snippets within your code editor
+- 📝 Quickly insert commonly used code snippets with just a few keystrokes
+- 📈 Save time and improve your coding efficiency
+
+## Demo 📽
+![demo](demo.gif)
+
+## Installation 📦
+
+1. Open VS Code and click on the Extensions icon in the left sidebar
+2. Search for "GistSnip" in the Extensions Marketplace
+3. Click on the Install button for GistSnip
+4. Once installed, click on the Activate button to enable the extension
+5. Restart VS Code to complete the installation process
---
+++ src/extension.ts
@@ -0,0 +1,80 @@
+import * as vscode from 'vscode';
+import { Credentials } from './credentials';
+
+export async function activate(context: vscode.ExtensionContext) {
+	const credentials = new Credentials();
+	await credentials.initialize(context);
+
+	const octokit = await credentials.getOctokit();
+
+	const disposable = vscode.commands.registerCommand('extension.authenticateWithGithub', async () => {
+		const userInfo = await octokit.users.getAuthenticated();
+
+		vscode.window.showInformationMessage(`Logged into GitHub as ${userInfo.data.login}`);
+	});
+
+	const items: vscode.CompletionItem[] = [];
+
+	const [{data: gistsOfOwn}, {data: gistsStared}] = await Promise.all([
+		octokit.gists.list(),
+		octokit.gists.listStarred()
+	]);
+	const gists = [...gistsOfOwn, ...gistsStared];
+	
+	for (let i = 0; i < (gists.length); i++) {
+		const { data: gist } = await octokit.gists.get({
+			gist_id: gists[i].id,
+		});
+
+		if (!gist || !(gist.files) || !(gist?.owner)) {
+			continue;
+		}
+
+		const files = Object.values(gist.files);
+		
+		for (let j = 0; j < (files.length); j++) {
+			const file = files[j];
+
+			if (!file || !(file.filename)) {
+				continue;
+			}
+
+			const completionItem = new vscode.CompletionItem({
+				label: file.filename,
+				detail: files[0]?.filename === file.filename ?  undefined : ` part of ${files[0]?.filename}`,
+				description: `${gist.owner.login}`,
+			});
+			completionItem.insertText = new vscode.SnippetString(file.content);
+			completionItem.kind = vscode.CompletionItemKind.File;
+
+			const documentationMarkdown = new vscode.MarkdownString();
+			documentationMarkdown.appendMarkdown(`**[${file.filename}](${gist.html_url})**`);
+			documentationMarkdown.appendText("\n");
+			if (gist.description) {
+				documentationMarkdown.appendMarkdown(`${gist.description}`);
+				documentationMarkdown.appendText("\n");
+			}
+			documentationMarkdown.appendMarkdown(`\`By\` **[${gist.owner.login}](${gist.owner.html_url})**`);
+			documentationMarkdown.appendText("\n");
+			documentationMarkdown.appendMarkdown(`\`Created on\` *${gist.created_at}*`);
+			documentationMarkdown.appendText("\n\n");
+			documentationMarkdown.appendCodeblock(`${file.content}`, file.language);
+
+			completionItem.documentation = documentationMarkdown;
+			
+			items.push(completionItem);
+		}
+	}
+	
+	const completionProvider = vscode.languages.registerCompletionItemProvider(
+		'*',
+		{
+			async provideCompletionItems() {
+				return items;
+			}
+		}
+	);
+
+	context.subscriptions.push(disposable);
+	context.subscriptions.push(completionProvider);
+}
